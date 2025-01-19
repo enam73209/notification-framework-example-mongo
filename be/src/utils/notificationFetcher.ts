392fac955a6a9f23ff7f74402766bedd94dd823c
Fahim Hoque <fahimhoque@Fahims-MacBook-Pro.local>
2025-01-19 22:40:55 +0600

    removed unnecessary method from util

--- be/src/utils/notificationFetcher.ts
+++ be/src/utils/notificationFetcher.ts
@@ -1,28 +1,21 @@
 import MockIds from "@/mock/mockIds";
-import { ExampleNotificationFrameworkClient as client } from "../clients/notificationClient";
-import { NotificationService } from "../notifications/lib/services/notification.service";
-import { UserNotificationMetadataService } from "../notifications/lib/services/userNotificationMetadata.service";
+import { 
+  ExampleNotificationFrameworkClient as client,
+} from "../clients/notificationClient";
+import {
+  NotificationService,
+} from "../notifications/lib/services/notification.service";
 
 export class NotificationServiceFetcher {
-  private readonly userId: string;
+  private readonly viewerUid: string;
   constructor(userId: string) {
-    this.userId = userId;
-  }
-
-  fromUserUid(userId: string): NotificationServiceFetcher {
-    return new NotificationServiceFetcher(userId);
+    this.viewerUid = userId;
   }
 
   async fetchNotificationService(): Promise<NotificationService> {
-    console.log("Fetching notification service for user", this.userId);
     const notifFramework = await client.getInstanceX();
     return notifFramework.getNotificationServiceX(
-      MockIds.getInstance().ownerUid1,
+      this.viewerUid,
     );
-  }
-
-  async fetchUserNotificationMetadataService(): Promise<UserNotificationMetadataService> {
-    const notifFramework = await client.getInstanceX();
-    return notifFramework.getUserNotificationMetadataServiceX(this.userId);
   }
 }
